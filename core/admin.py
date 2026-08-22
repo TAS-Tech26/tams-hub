4d1c633fdd4e956eb763c8b3d55df28b99ddc831
Srimannarayan Paidisetty <78963283+SrimannarayanP@users.noreply.github.com>
2026-08-23 00:13:49 +0530

    Registered models & configured EventRoom admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
+# admin.py
+
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import EventRoom, EventStanding, School, TamsTeam
+
+
+@admin.register(EventRoom)
+class EventRoomAdmin(admin.ModelAdmin):
+
+    list_display = ('event_name', 'room_code', 'is_active')
+    list_filter = ('event_name', 'is_active')
+
+    search_fields = ('room_code', 'event_name')
+
+admin.site.register(School)
+admin.site.register(TamsTeam)
+admin.site.register(EventStanding)
